e011fd91e18ddec901c1ef8d9bdb7291b56fdcbb
JuanCruzBarberisSAX <juanbarberis.97@gmail.com>
2024-05-10 11:40:07 -0300

    menu hamburguesa corregido

--- src/components/menuHamburguesa/MenuHamburguesa.jsx
+++ src/components/menuHamburguesa/MenuHamburguesa.jsx
@@ -5,13 +5,12 @@ import './menuhamburguesa.css'
 
 const MenuHamburguesa = ({ handleRender }) => {
     return (
-        <div className='nav-container-hamburguesa active'>
-            {/* <CgClose onClick={handleRender} className='close-menu' /> */}
-            <div className='profile'>
-                <Link activeClass="active" to="inicio" spy={true} smooth={true} offset={0} duration={800} className='link-menu-hamburguesa' >Inicio</Link>
-                <Link activeClass="active" to="quienes-somos" spy={true} smooth={true} offset={0} duration={800} className='link-menu-hamburguesa'  >Quienes Somos</Link>
-                <Link activeClass="active" to="proyectos" spy={true} smooth={true} offset={0} duration={800} className='link-menu-hamburguesa'  >Proyectos</Link>
-                <Link activeClass="active" to="contacto" spy={true} smooth={true} offset={0} duration={800} className='link-menu-hamburguesa'  >Contacto</Link>
+        <div className='nav-container-hamburguesa active' >
+            <div className='profile'onClick={handleRender}>
+                <Link activeClass="active" onClick={handleRender}  to="inicio" spy={true} smooth={true} offset={0} duration={800} className='link-menu-hamburguesa' >Inicio</Link>
+                <Link activeClass="active" onClick={handleRender} to="quienes-somos" spy={true} smooth={true} offset={0} duration={800} className='link-menu-hamburguesa'  >Quienes Somos</Link>
+                <Link activeClass="active" onClick={handleRender} to="proyectos" spy={true} smooth={true} offset={0} duration={800} className='link-menu-hamburguesa'  >Proyectos</Link>
+                <Link activeClass="active" onClick={handleRender} to="contacto" spy={true} smooth={true} offset={0} duration={800} className='link-menu-hamburguesa'  >Contacto</Link>
             </div>
         </div >
     )
