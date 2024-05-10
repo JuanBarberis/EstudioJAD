9b58915c5f56976582704831b57dcbe5d5711ae5
JuanCruzBarberisSAX <juanbarberis.97@gmail.com>
2024-05-10 11:40:22 -0300

    se corrigo nav bar

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -4,17 +4,15 @@ import logo from '../../assets/imagenes/logo.png'
 import { Link } from 'react-scroll';
 import MenuHamburguesa from '../menuHamburguesa/MenuHamburguesa';
 import { FiMenu, FiX } from "react-icons/fi";
+import { IoMenu, IoClose } from "react-icons/io5";
 
 export const Header = () => {
 
-    const [render, setRender] = useState(false);
-    const [navbar, setNavnar] = useState(false)
     const [isOpen, setIsOpen] = useState(false);
 
     const handleRender = () => {
         // setRender(!render)
         setIsOpen(!isOpen);
-        console.log(isOpen)
     }
 
 
@@ -23,8 +21,18 @@ export const Header = () => {
             <Link to="inicio" spy={true} smooth={true} duration={800} className='img-logo'>
                 <img src={logo} alt='img-logo' width={200} height={100} />
             </Link>
-            {isOpen ? <FiX onClick={handleRender} className='menuhamburguesa' /> : <FiMenu onClick={handleRender} className='menuhamburguesa' />}
-            {isOpen ? <MenuHamburguesa handleRender={handleRender} /> : ' '}
+            {
+                isOpen ?
+                    <div className='div-menuhamburguesa'>
+                        <IoClose onClick={handleRender} className='menuhamburguesa' />
+                    </div>
+                    :
+                    <div className='div-menuhamburguesa'>
+                        <IoMenu onClick={handleRender} className='menuhamburguesa' />
+                    </div>
+            }
+            {isOpen && <MenuHamburguesa handleRender={handleRender} />}
+
             <div className='link-container'>
                 <Link className='link-path' to='inicio' spy={true} smooth={true} offset={0} duration={800}>Inicio</Link>
                 <Link to='quienes-somos' className='link-path' spy={true} smooth={true} offset={0} duration={800}>Quienes Somos</Link>
