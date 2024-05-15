2070c1d97997861de4b75dc58c8a17b8971417a9
JuanCruzBarberisSAX <juanbarberis.97@gmail.com>
2024-05-15 20:59:31 -0300

    se modifico el layout para redireccionar las vistas

--- src/layouts/MainLayouts.jsx
+++ src/layouts/MainLayouts.jsx
@@ -4,15 +4,16 @@ import Home from '../pages/home/Home'
 import AboutUs from '../pages/about/AboutUs'
 import Proyects from '../pages/proyects/Proyects'
 import Contact from '../pages/contact/Contact'
+import Servicios from '../pages/servicios/Servicios'
+import { Outlet } from 'react-router-dom'
+import ProyectDetails from '../pages/proyects/proyectDetails/ProyectDetails'
 
 export const MainLayouts = () => {
     return (
         <>
-            <Header />
-            <Home />
-            <AboutUs />
-            <Proyects />
-            <Contact />
+            {/* <Header /> */}
+            <Outlet />
+
         </>
     )
 }
