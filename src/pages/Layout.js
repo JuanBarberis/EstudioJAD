42d8e7e37893993e5ea7a8ba5e72cb98852e36ea
JuanCruzBarberisSAX <juanbarberis.97@gmail.com>
2024-05-15 20:59:51 -0300

    se modifico

--- src/pages/Layout.js
+++ src/pages/Layout.js
@@ -5,16 +5,27 @@ import Home from './home/Home'
 import AboutUs from './about/AboutUs'
 import Proyects from './proyects/Proyects'
 import Contact from './contact/Contact'
+import Servicios from './servicios/Servicios'
+import ProyectDetails from './proyects/proyectDetails/ProyectDetails'
+import LandingPage from '../layouts/LandingPage'
 
 export const router = createBrowserRouter([
     {
         path: '/',
-        element: <MainLayouts />,
+        element: <LandingPage />,
         children: [
             { path: 'inicio', element: <Home /> },
             { path: 'quienes-somos', element: <AboutUs /> },
-            { path: 'proyectos', element: <Proyects /> },
+            { path: 'servicios', element: <Servicios /> },
             { path: 'contacto', element: <Contact /> },
+            { path: 'proyectos', element: <Proyects /> },
+        ]
+    },
+    {
+        path: '/',
+        element: < MainLayouts />,
+        children: [
+            { path: '/proyectos/:proyectId', element: <ProyectDetails /> }
         ]
     }
 ])
