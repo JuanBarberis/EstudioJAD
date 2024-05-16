e38c1c9c05efe62cfb15bcd255183e07bc652223
JuanCruzBarberisSAX <juanbarberis.97@gmail.com>
2024-05-15 21:00:12 -0300

    se agrego la seccion servicios

--- src/components/menuHamburguesa/MenuHamburguesa.jsx
+++ src/components/menuHamburguesa/MenuHamburguesa.jsx
@@ -17,6 +17,7 @@ const MenuHamburguesa = ({ handleRender }) => {
             <div className='profile' onClick={handleRender}>
                 <Link activeClass="active" onClick={handleRender} to="inicio" spy={true} smooth={true} offset={0} duration={800} className='link-menu-hamburguesa' >Inicio</Link>
                 <Link activeClass="active" onClick={handleRender} to="quienes-somos" spy={true} smooth={true} offset={0} duration={800} className='link-menu-hamburguesa'  >Quienes Somos</Link>
+                <Link activeClass="active" onClick={handleRender} to="servicios" spy={true} smooth={true} offset={0} duration={800} className='link-menu-hamburguesa'  >Servicios</Link>
                 <Link activeClass="active" onClick={handleRender} to="proyectos" spy={true} smooth={true} offset={0} duration={800} className='link-menu-hamburguesa'  >Proyectos</Link>
                 <Link activeClass="active" onClick={handleRender} to="contacto" spy={true} smooth={true} offset={0} duration={800} className='link-menu-hamburguesa'  >Contacto</Link>
             </div>
