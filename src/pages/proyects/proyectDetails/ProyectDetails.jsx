77b0fa60602f6c4e549c9b484bbab520ef097683
JuanCruzBarberis <juanbarberis.97@gmail.com>
2024-05-22 22:38:04 -0300

    se modifico el header para que sea oscuro

--- src/pages/proyects/proyectDetails/ProyectDetails.jsx
+++ src/pages/proyects/proyectDetails/ProyectDetails.jsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 import './proyectDetails.css'
 import { useNavigate, useParams } from 'react-router-dom';
 import Imagen1 from '../../../assets/imagenes/imagen1.jpg'
@@ -20,19 +20,30 @@ const ProyectDetails = () => {
         3: { nombre: 'Proyecto 3', descripcion: 'Descripción del Proyecto 3', imagen: Imagen3 },
         4: { nombre: 'Proyecto 4', descripcion: 'Descripción del Proyecto 4', imagen: Imagen4 },
         5: { nombre: 'Proyecto 5', descripcion: 'Descripción del Proyecto 5', imagen: Imagen5 },
+        6: { nombre: 'Proyecto 6', descripcion: 'Descripción del Proyecto 6', imagen: Imagen1 },
+        7: { nombre: 'Proyecto 7', descripcion: 'Descripción del Proyecto 7', imagen: Imagen2 },
+        8: { nombre: 'Proyecto 8', descripcion: 'Descripción del Proyecto 8', imagen: Imagen3 },
+        9: { nombre: 'Proyecto 9', descripcion: 'Descripción del Proyecto 9', imagen: Imagen4 },
+        10: { nombre: 'Proyecto 10', descripcion: 'Descripción del Proyecto 10', imagen: Imagen5 },
     };
-  
+
     const project = projectData[projectId.proyectId];
 
+    useEffect(() => {
+        window.scrollTo(0, 0);
+    }, []);
+    
     if (!project) {
         return <div>Proyecto no encontrado</div>;
     }
+
+
     return (
         <div className='proyect-detail'>
             <h1>{project.nombre}</h1>
             <img src={project.imagen} alt={project.nombre} />
             <p>{project.descripcion}</p>
-            <button onClick={()=> navigate(-1)}>Volver atras</button>
+            <button onClick={() => navigate(-1)}>Volver atras</button>
         </div>
     )
 }
