2d3bf90fa07aa7a5be101a6043b051c0494d3d0d
JuanCruzBarberis <juanbarberis.97@gmail.com>
2024-05-29 15:39:29 -0300

    se le dio estilo a vista construccion

--- src/pages/construction/Construction.jsx
+++ src/pages/construction/Construction.jsx
@@ -8,6 +8,7 @@ import imagen4 from '../../assets/imagenes/imagen4.jpg'
 import imagen5 from '../../assets/imagenes/imagen5.jpg'
 import BotonWhatsapp from '../../components/whatsapp/BotonWhatsap'
 import { Link as ScrollLink } from 'react-scroll';
+import Divider from '../../components/divider/Divider'
 
 const Construction = () => {
 
@@ -30,41 +31,28 @@ const Construction = () => {
 
   ]
 
-  const data = [
+  const infoServicio = [
     {
       id: 1,
-      title: 'Desde Cero',
-      subtitle: '',
-      description: ' Lorem ipsum dolor sit amet consectetur, adipisicing elit. Officiis, incidunt.',
-      image: imagen1
-    },
-    {
-      id: 2,
-      title: 'Ante Proyecto',
-      subtitle: 'Proceso Creativo',
-      description: ' Lorem ipsum dolor sit amet consectetur, adipisicing elit. Officiis, incidunt.',
-      image: imagen2
-    },
-    {
-      id: 3,
-      title: 'Proyecto',
-      subtitle: 'Etapa de formalizacion',
-      description: ' Lorem ipsum dolor sit amet consectetur, adipisicing elit. Officiis, incidunt.',
-      image: imagen3
-    },
-    {
-      id: 4,
-      title: 'Construcción',
-      subtitle: 'Etapa de concretación',
-      description: ' Lorem ipsum dolor sit amet consectetur, adipisicing elit. Officiis, incidunt.',
-      image: imagen5
-    },
-    {
-      id: 5,
-      title: 'Final de obra',
-      subtitle: 'Momento de disfrutar',
-      description: ' Lorem ipsum dolor sit amet consectetur, adipisicing elit. Officiis, incidunt.',
-      image: imagen4
+      title: 'CONTRATACION LLAVE EN MANO',
+      subtitle: [
+        {
+          id: 1,
+          title: 'PROCESO DE EJECUCION DE OBRA',
+          subtitle: '',
+          description: [
+            'Computo Métrico de Materiales',
+            'Presupuesto de Obra',
+            'Plan de Avance',
+            'Planificación  de inversión por Etapas',
+            'Tramites preliminares para el inicio de obra',
+            'Administración de Materiales',
+            'Equipos de Trabajos Especializados para cada Etapa',
+            'Dirección Técnica '
+          ]
+        },
+      ],
+      imagen: imagen1,
     },
   ]
 
@@ -97,24 +85,69 @@ const Construction = () => {
             <button className='boton-presupuesto'>Pedi tu presupuesto</button>
           </ScrollLink>
         </div>
-        <div className='div-container-arquitecture' >
-          {
-            data.map((item, index) => {
-              return (
-                <>
-                  <div className={item.id % 2 === 0 ? 'div-container-draft' : 'div-container-draft2'}>
-                    <div className='div-contain-info'>
-                      <div className='div-contain-title'>
-                        <p className='text-title-arquitecture'>{item.title}</p>
-                        <p className='text-subtitle-arquitecture'> {item.subtitle} </p>
+        <div className='container-presentacion-construccion'>
+          <div className='container-presentacion-h3-h6'>
+            <p className='p-presentacion-construccion'>Te acompañamos desde el inicio hasta el final de todo el proceso, con una propuesta integral que se amolda independientemente a la etapa en la que se encuentre tu proyecto. Nuestro servicios dentro de Construccion
+            </p>
+          </div>
+          <div className='container-presentacion-arquitectura-p'>
+            <Divider thickness={"1.5px"} height={"30px"} color={"black"} direction={"vertical"} />
+            <ScrollLink to="CONTRATACION LLAVE EN MANO" spy={true} smooth={true} offset={-70} duration={800}>
+              <p className='p-servicios-arquitectura'>CONTRATACION LLAVE EN MANO</p>
+            </ScrollLink>
+            <Divider thickness={"1.5px"} height={"30px"} color={"black"} direction={"vertical"} />
+
+          </div>
+
+        </div>
+        <div className='div-container-construccion' >
+          {infoServicio.map((item, index) => {
+            return (
+              <>
+                <div
+                  id={item.title}
+                  key={index}
+                  className={`div-container-item ${index % 2 !== 0 ? 'div-container-item-reverse' : ''
+                    }`}
+                >
+
+                  <div className='div-contain-info-construccion'>
+                    {
+                      item.id === 1 &&
+                      (
+                        <div key={index} className='container-construccion-propiedad'>
+                          <p className='p-construccion-propiedad'>Si contas con un Proyecto de Arquitectura o Refaccion y queres comenzar con la construccion te recomendamos el servicio de Sistema de Contratacion Llave en mano que te detallamos debajo</p>
+                        </div>
+                      )
+                    }
+                    <h1 className='title-construccion'>{item.title}</h1>
+                    {item.id != 2 &&
+                      (<Divider width={'100%'} />)
+                    }
+                    {item.subtitle.map((subItem, subIndex) => (
+                      <div className='info-detallada-construccion' key={subIndex}>
+                        <h2 className='h2-title-construccion'>{subItem.title}</h2>
+                        <p className={subItem.subtitle ? 'p-subtitle-construccion' : ''}>
+                          {subItem.subtitle}
+                        </p>
+                        {subItem.description.map((desc, descIndex) => (
+                          <p key={descIndex} className='descripcion-construccion'>
+                            {`- ${desc}`}
+                          </p>
+                        ))}
                       </div>
-                      <p className='text-describe-arquitecture'>{item.description}</p>
-                    </div>
-                    <img src={item.image}  className='imagen-arquitecture'/>
+                    ))}
                   </div>
-                </>
-              )
-            })
+                  <img
+                    src={item.imagen}
+                    alt={item.title}
+                    className='imagen-construccion'
+                  />
+                </div>
+              </>
+            )
+          })
+
           }
         </div>
       </div>
