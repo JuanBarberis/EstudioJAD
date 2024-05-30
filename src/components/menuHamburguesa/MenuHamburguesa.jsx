2398ac3a14d7381795d6ec0181a77ffb0f211a47
JuanCruzBarberis <juanbarberis.97@gmail.com>
2024-05-30 16:11:49 -0300

    se cambiaron estilos en el menu hamburguesa

--- src/components/menuHamburguesa/MenuHamburguesa.jsx
+++ src/components/menuHamburguesa/MenuHamburguesa.jsx
@@ -2,34 +2,33 @@ import React, { useState } from 'react'
 // import { Link } from 'react-scroll';
 import './menuhamburguesa.css'
 import { motion } from "framer-motion";
-import { Link, useNavigate } from 'react-router-dom'
+import { Link, useLocation, useNavigate, useParams } from 'react-router-dom'
 
 const MenuHamburguesa = ({ handleRender }) => {
 
     const navigate = useNavigate();
+    const location = useLocation();
 
-
-
-
+    const params = useParams()
+    const navParams = params.proyectId
     const handleLinkClick = (path, sectionId) => {
         navigate(path, { state: { sectionId } });
     };
 
+    const navClass = location.pathname === '/all-proyects'
+        ? 'nav-container-hamburguesa-black'
+        : navParams ? 'nav-container-hamburguesa-black'
+            : 'nav-container-hamburguesa';
+
     return (
 
         <motion.div
-            className='nav-container-hamburguesa active'
+            className={navClass}
             initial={{ opacity: 0, y: -50 }}
             animate={{ opacity: 1, y: 0 }}
             transition={{ duration: 0.5 }}
-        // exit={{ opacity: 0, y: -50 }} // Animación de salida
         >
             <div className='profile' onClick={handleRender}>
-                {/* <Link activeClass="active" onClick={handleRender} to="inicio" spy={true} smooth={true} offset={0} duration={800} className='link-menu-hamburguesa' >INICIO</Link>
-                <Link activeClass="active" onClick={handleRender} to="servicios" spy={true} smooth={true} offset={-100} duration={800} className='link-menu-hamburguesa'  >SERVICIOS</Link>
-                <Link activeClass="active" onClick={handleRender} to="proyectos" spy={true} smooth={true} offset={-100} duration={800} className='link-menu-hamburguesa'  >PORTFOLIO</Link>
-                <Link activeClass="active" onClick={handleRender} to="quienes-somos" spy={true} smooth={true} offset={-100} duration={800} className='link-menu-hamburguesa'  >ESTUDIO</Link>
-                <Link activeClass="active" onClick={handleRender} to="contacto" spy={true} smooth={true} offset={0} duration={800} className='link-menu-hamburguesa'  >CONTACTO</Link> */}
                 <button className='link-menu-hamburguesa' onClick={() => handleLinkClick('/', 'inicio')}>INICIO</button>
                 <button className='link-menu-hamburguesa' onClick={() => handleLinkClick('/', 'servicios')}>SERVICIOS</button>
                 <button className='link-menu-hamburguesa' onClick={() => handleLinkClick('/', 'proyectos')}>PORTFOLIO</button>
