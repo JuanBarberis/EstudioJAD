653508914c36e35c31d8fc799c8f13890b094d25
JuanCruzBarberis <juanbarberis.97@gmail.com>
2024-06-05 16:50:08 -0300

    se agregaron imagenes portada

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -2,10 +2,11 @@ import React from 'react'
 import './home.css'
 import "react-image-gallery/styles/css/image-gallery.css"
 import ImageGallery from 'react-image-gallery'
-import imagen1 from '../../assets/imagenes/imagen1.jpg'
-import imagen2 from '../../assets/imagenes/imagen2.jpg'
-import imagen3 from '../../assets/imagenes/imagen3.jpg'
-import imagen5 from '../../assets/imagenes/imagen5.jpg'
+import imagen1 from '../../assets/imagenes/Portada-home-1.jpg'
+import imagen2 from '../../assets/imagenes/Porta-home-2.jpg'
+import imagen3 from '../../assets/imagenes/Portada-home-3.jpg'
+import imagen4 from '../../assets/imagenes/Portada-home-4.jpg'
+import imagen5 from '../../assets/imagenes/Portada-home-5.jpg'
 import BotonWhatsapp from '../../components/whatsapp/BotonWhatsap'
 
 
@@ -15,22 +16,22 @@ const Home = () => {
         {
             original: imagen1,
             thumbnail: imagen1,
-            description: 'ARQUITECTURA'
         },
         {
             original: imagen2,
             thumbnail: imagen2,
-            description: 'INTERIORISMO'
         },
         {
             original: imagen3,
             thumbnail: imagen3,
-            description: 'CONSTRUCCION '
+        },
+        {
+            original: imagen4,
+            thumbnail: imagen4,
         },
         {
             original: imagen5,
             thumbnail: imagen5,
-            description: 'SOMOS ESTUDIO JAD '
         },
 
     ]
@@ -53,7 +54,7 @@ const Home = () => {
                 )}
             />
             <div className='div-text-welcome'>
-                <p className='texto-presentacion'>{'CREAMOS ESPACIOS QUE POTENCIAN TUS SENTIDOS'}</p>
+                <p className='texto-presentacion'>{'Creamos espacios que potencian tus sentidos'}</p>
                 <p className='texto-bienvenida'>{'Bienvenidos, te invitamos a pasar.'}</p>
             </div>
 
